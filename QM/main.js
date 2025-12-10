57a1efe6a5ac5a088295f81e729219211f911312
“MariaJGamboa” <mariajuliana.gamboa@studio.unibo.it>
2025-12-10 15:46:21 +0100

    main

--- QM/main.js
+++ QM/main.js
@@ -1,1 +1,18 @@
-function 
+//javascript per cambiare gli stili//
+
+function changeTheme(cssFile) {
+        const link = document.getElementById("theme-css");
+        if(link) {
+      // cambia il tema e forza ricaricamento senza cache
+          link.href = cssFile + "?v=" + Date.now();
+          sessionStorage.setItem('selectedTheme', cssFile)
+    }
+      }
+  // Applica il tema salvato al caricamento della pagina
+        document.addEventListener('DOMContentLoaded', () => {
+          const savedTheme = sessionStorage.getItem('selectedTheme');
+          if(savedTheme) {
+            const link = document.getElementById("theme-css");
+            if(link) link.href = savedTheme;
+          }
+        });
